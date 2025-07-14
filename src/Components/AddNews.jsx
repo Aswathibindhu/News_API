bd09ef650ec05befcdf32aa0c02d70a471c16e3b
Aswathibindhu <aswathibindhu4.com>
2025-07-14 15:48:33 +0530

    Created

---
+++ src/Components/AddNews.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import NavigationBar from './NavigationBar'
+
+const AddNews = () => {
+    return (
+        <div>
+            <NavigationBar/>
+            <div className="container">
+                <div className="row col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row">
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <div class="card">
+                                <img src="..." class="card-img-top" alt="..."/>
+                                    <div class="card-body">
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                                    </div>
+                            </div>
+                        </div>
+                    </div>git
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default AddNews
